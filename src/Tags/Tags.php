c8360f9f4138d57764b0e11e3d27799db54fc58a
Coen Van der kolk <coen@MacBook-Pro-van-Coen.local>
2020-06-08 13:41:30 +0200

    Added tags

--- src/Tags/Tags.php
+++ src/Tags/Tags.php
@@ -11,6 +11,79 @@ use Coennie\ActiveCampaign\Resource;
  */
 class Tags extends Resource
 {
+	/**
+	 * Create a tag
+	 * @see https://developers.activecampaign.com/reference#create-a-new-tag
+	 *
+	 * @param array $tag
+	 * @return string
+	 */
+	public function create(array $tag)
+	{
+		$req = $this->client
+			->getClient()
+			->post('/api/3/tags', [
+				'json' => [
+					'tag' => $tag
+				]
+			]);
+
+		return $req->getBody()->getContents();
+	}
+
+	/**
+	 * Get tag
+	 * @see https://developers.activecampaign.com/reference#retrieve-a-tag
+	 *
+	 * @param int $id
+	 *
+	 * @return string
+	 */
+	public function get(int $id)
+	{
+		$req = $this->client
+			->getClient()
+			->get('/api/3/tags/' . $id);
+
+		return $req->getBody()->getContents();
+	}
+
+	/**
+	 * Update a tag
+	 * @see https://developers.activecampaign.com/reference#update-a-tag
+	 *
+	 * @param int $id
+	 * @param array $tag
+	 * @return string
+	 */
+	public function update(int $id, array $tag)
+	{
+		$req = $this->client
+			->getClient()
+			->put('/api/3/tags/' . $id, [
+				'json' => [
+					'tag' => $tag
+				]
+			]);
+
+		return $req->getBody()->getContents();
+	}
+
+	/**
+	 * Delete a tag
+	 * @see https://developers.activecampaign.com/reference#remove-a-tag
+	 *
+	 * @param int $id
+	 * @return string
+	 */
+	public function delete(int $id)
+	{
+		$req = $this->client
+			->getClient()
+			->delete('/api/3/tags/' . $id);
+
+		return 200 === $req->getStatusCode();
+	}
 
     /**
      * List all tags
@@ -25,7 +98,6 @@ class Tags extends Resource
             ->get('/api/3/tags', [
                 'query' => $query_params
             ]);
-
         return $req->getBody()->getContents();
     }
 
